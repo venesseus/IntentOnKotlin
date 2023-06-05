a4b08b48f682cc8611bec17388c45b9a54773d7b
satri <satriayusufrafaly@gmail.com>
2023-06-05 19:24:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_intent_satriayusuf/MoveForResultActivity.kt
@@ -0,0 +1,52 @@
+package com.example.android_intent_satriayusuf
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat.RegisterReceiverFlags
+
+class MoveForResultActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnChoose: Button
+    private lateinit var rgNumber: RadioGroup
+
+    companion object {
+        const val EXTRA_SELECTED_VALUE = "extra_selected_value"
+        const val RESULT_CODE = 110
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_for_result)
+
+        btnChoose = findViewById(R.id.btn_choose)
+        rgNumber = findViewById(R.id.rg_number)
+
+        btnChoose.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_choose) {
+            if (rgNumber.checkedRadioButtonId > 0) {
+                var value = 0
+                when (rgNumber.checkedRadioButtonId) {
+                    R.id.rb_50 -> value = 50
+                    R.id.rb_100 -> value = 100
+                    R.id.rb_150 -> value = 150
+                    R.id.rb_200 -> value = 200
+                }
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_SELECTED_VALUE, value)
+                setResult(RESULT_CODE, resultIntent)
+                finish()
+            }
+        }
+    }
+}
